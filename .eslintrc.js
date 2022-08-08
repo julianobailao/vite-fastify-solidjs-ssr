3924ef79cf14cb13b62e862bbb9499df29ddc8b4
Juliano Bailão <julianobailao@gmail.com>
2022-08-08 13:17:09 -0300

    ESLint config set to solidjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,8 @@ module.exports = {
     browser: true,
     node: true,
     es2021: true,
-    "jest/globals": true,
   },
-  extends: ["plugin:react/recommended", "prettier", "eslint:recommended"],
+  extends: ["plugin:solid/recommended", "prettier", "eslint:recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -14,13 +13,11 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["jest", "react", "@typescript-eslint"],
+  plugins: ["solid", "@typescript-eslint"],
   rules: {
     // note you must disable the base rule as it can report incorrect errors
     "no-use-before-define": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-use-before-define": ["error"],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
   },
 };
