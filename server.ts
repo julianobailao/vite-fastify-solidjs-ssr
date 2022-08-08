2cf2180c14351fe3b80ef4e82263c3c7ea7c599a
Juliano Bailão <julianobailao@gmail.com>
2022-08-08 13:19:03 -0300

    Improve server implementation

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
-import createServer from "./app";
+import bootstrap from "./app";
 
-createServer().then(({ app }) => {
+async function createServer(isProd = process.env.NODE_ENV === "production") {
+  const app = await bootstrap(isProd);
   const port = process.env.PORT || 7456;
   app.listen(Number(port), "0.0.0.0", () => {
     console.log(`App is listening on http://localhost:${port}`);
   });
-});
+}
+
+createServer();
