6cc86b2a712796a1c340021a7ce2f5ad12fa60a9
Juliano Bailão <julianobailao@gmail.com>
2022-08-09 17:17:40 -0300

    Server now exports fastify app and not up on tests env

--- server.ts
+++ server.ts
@@ -1,11 +1,14 @@
 import bootstrap from "./app";
 
-async function createServer(isProd = process.env.NODE_ENV === "production") {
+export async function createServer(isProd = process.env.NODE_ENV === "production") {
   const app = await bootstrap(isProd);
   const port = process.env.PORT || 7456;
-  app.listen(Number(port), "0.0.0.0", () => {
+  await app.listen(Number(port), "0.0.0.0", () => {
     console.log(`App is listening on http://localhost:${port}`);
   });
+
+  return app;
 }
 
-createServer();
+const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD;
+if (!isTest) createServer();
