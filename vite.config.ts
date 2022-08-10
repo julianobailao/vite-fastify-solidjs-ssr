7826fd28e81bc4f072dde8170c326e1e00cb5860
Juliano Bailão <julianobailao@gmail.com>
2022-08-09 21:41:59 -0300

    Added vite compression with brotli / gzip

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import compress from "vite-plugin-compress";
+
+const isProd = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [solid({ ssr: true })],
+  plugins: [isProd && compress({ verbose: true }), solid({ ssr: true })],
   server: { port: 3000 },
   build: {
     minify: false,
