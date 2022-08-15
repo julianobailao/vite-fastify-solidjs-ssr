0c247ef4e362231df4d6d1e34cdcf4fd43266bac
Juliano Bailão <julianobailao@gmail.com>
2022-08-15 13:09:28 -0300

    #4 - Adding initial way to make internal request only on client

--- src/client/components/AjaxDataLoader.tsx
+++ src/client/components/AjaxDataLoader.tsx
@@ -1,10 +1,23 @@
-import axios from "axios";
+import { ExampleService } from "@server/services/example.service";
+import { ORIGIN } from "@shared/enums/origin.enum";
 import { createResource } from "solid-js";
+import { isServer } from "solid-js/web";
 
 function createDelay() {
   const startedAt = new Date();
   return new Promise(resolve => {
-    setTimeout(() => axios.get("http://localhost:7456/api").then(({ data }) => resolve({ startedAt, ...data })), 3500);
+    setTimeout(() => {
+      if (isServer) {
+        const service = new ExampleService();
+        resolve({ startedAt, isServer, ...service.getData(ORIGIN.SERVER) });
+      } else {
+        fetch("http://localhost:7456/api")
+          .then(response => response.json())
+          .then(data => {
+            resolve({ startedAt, isServer, ...data });
+          });
+      }
+    }, 3500);
   });
 }
 
